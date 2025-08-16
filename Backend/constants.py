94fdf3172cced5fb1107c34d028cba793e11d72a
Shashank-Script <shashankgupta26122@gmail.com>
2025-08-16 17:44:53 +0530

    preparing for deployment

--- Backend/constants.py
+++ Backend/constants.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
-PORT = '8900'
-ENV = 'dev'
+SERVER_URL = '0.0.0.0'
+PORT = os.getenv('PORT','8900')
+ENV = os.getenv('ENV','dev')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
